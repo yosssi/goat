035f208e6de98c2e87273a0b5793195a02201093
Nithin Bose <nithin.bose@askme.in>
2016-02-04 00:43:57 +0530

    updated test files for yaml config files. Ref #2

--- context/config_test.go
+++ context/config_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestNewConfig(t *testing.T) {
 	// When ioutil.ReadFile returns an error.
-	expectedErrMsg := "open goat.json: no such file or directory"
+	expectedErrMsg := "open goat.yml: no such file or directory"
 	_, err := NewConfig()
 	if err == nil || err.Error() != expectedErrMsg {
 		t.Errorf("Error (%s) should be returned.", expectedErrMsg)
@@ -21,9 +21,28 @@ func TestNewConfig(t *testing.T) {
 		t.Errorf("Error (%s) should be returned.", expectedErrMsg)
 	}
 
-	// When config is returned.
+	// When config from json is returned.
 	os.Chdir(os.Getenv("GOPATH") + "/src/github.com/yosssi/goat/test/context/TestNewConfig002")
 	config, err := NewConfig()
+	if err != nil {
+		t.Errorf("Error (%s) occurred.", err.Error())
+	}
+	if len(config.Watchers) != 1 || config.Watchers[0].Extension != "go" || len(config.Watchers[0].Tasks) != 1 || config.Watchers[0].Tasks[0].Command != "make rerun" || config.Watchers[0].Tasks[0].Nowait != true {
+		t.Errorf("Config is invalid.")
+	}
+
+	// When yaml.Unmarshal returns an error.
+	expectedErrMsg = "yaml: line 1: did not find expected node content"
+	os.Chdir(os.Getenv("GOPATH") + "/src/github.com/yosssi/goat/test/context/TestNewConfig003")
+	_, err = NewConfig()
+	if err == nil || err.Error() != expectedErrMsg {
+		t.Errorf("Error returned (%s)", err.Error())
+		t.Errorf("Error (%s) should be returned.", expectedErrMsg)
+	}
+
+	// When config from yaml is returned.
+	os.Chdir(os.Getenv("GOPATH") + "/src/github.com/yosssi/goat/test/context/TestNewConfig004")
+	config, err = NewConfig()
 	if err != nil {
 		t.Errorf("Error (%s) occurred.", err.Error())
 	}
